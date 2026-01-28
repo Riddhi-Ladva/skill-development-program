1055bdd1251acae29fdc637b39c5166df6d153d8
Riddhi Ladva <rid.ladvq@gmail.com>
2026-01-28 12:00:01 +0530

    feat:add shipping methods

--- easycart/pages/set-shipping.php
+++ easycart/pages/set-shipping.php
@@ -19,10 +19,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'price' => $prices[$type]
         ];
 
+        // Calculate Totals to return
+        require_once '../data/products.php';
+        $subtotal = 0;
+        if (isset($_SESSION['cart'])) {
+            foreach ($_SESSION['cart'] as $id => $quantity) {
+                if (isset($products[$id])) {
+                    $subtotal += $products[$id]['price'] * $quantity;
+                }
+            }
+        }
+        $shipping = $prices[$type];
+        $tax = $subtotal * 0.08;
+        $total = $subtotal + $shipping + $tax;
+
         echo json_encode([
             'success' => true,
             'type' => $type,
-            'price' => $prices[$type]
+            'totals' => [
+                'subtotal' => '$' . number_format($subtotal, 2),
+                'shipping' => $shipping == 0 ? 'FREE' : '$' . number_format($shipping, 2),
+                'tax' => '$' . number_format($tax, 2),
+                'grandTotal' => '$' . number_format($total, 2)
+            ]
         ]);
     } else {
         echo json_encode(['success' => false, 'message' => 'Invalid shipping type']);
